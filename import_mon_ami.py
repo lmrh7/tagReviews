2508291a71bd97b408f398f86ff685b46e4c46a3
crobertob <crobertob@hotmail.com>
2016-03-05 13:20:27 -0700

    Initial Commit
    Added scripts that convert from json to pickle and an example of how to import pickled data. The drill commands can be used with apache drill.

---
+++ import_mon_ami.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+mon_ami_photos = pd.read_pickle(r'C:\Users\crobe\Google Drive\DataMiningGroup\Datasets\mon_ami_gabi_photos.pkl')
+mon_ami_reviews = pd.read_pickle(r'C:\Users\crobe\Google Drive\DataMiningGroup\Datasets\mon_ami_gabi_reviews.pkl')
